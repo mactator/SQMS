63ac13519cab7a503e994d10f41a398fdc92b215
Abdullrahman <Abdullrahman.chaabouni123@gmail.com>
2024-11-02 20:58:30 +0300

    style: Dark mode fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark" data-theme='dark'>
       <body>
         <Background />
         <Header />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  darkMode: "selector",
+
   theme: {},
   plugins: [require("daisyui"), require("tailwindcss-animate")],
   daisyui: {
     themes: [
       {
-        winter: {
+        dark: {
           ...require("daisyui/src/theming/themes")["dark"],
           primary: "#2563eb",
         },
